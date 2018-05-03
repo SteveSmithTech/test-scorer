dde2d878016aceb59949c00577ec1a7ab15c9449
Steve Smith <steve@continuousdeliveryconsulting.com>
2018-05-03 16:00:13 +0100

    Test reports parser

--- src/test/java/com/github/stevesmithcd/testscorer/parser/TestReportsParserTest.java
+++ src/test/java/com/github/stevesmithcd/testscorer/parser/TestReportsParserTest.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.util.List;
 
 import static com.github.stevesmithcd.testscorer.domain.Result.SUCCESS;
+import static com.github.stevesmithcd.testscorer.parser.FileFilters.isSerenityResultsDirectory;
 import static java.time.LocalDateTime.now;
 import static java.util.Arrays.asList;
 import static org.hamcrest.CoreMatchers.is;
@@ -17,16 +18,16 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-public class SerenityTestReportsParserTest {
+public class TestReportsParserTest {
 
     @SuppressWarnings("unchecked")
-    private Parser<TestReport> serenityTestReportParser = mock(Parser.class);
+    private Parser<TestReport> testReportParser = mock(Parser.class);
 
-    private final Parser<List<TestReport>> serenityTestReportsParser = new SerenityTestReportsParser(serenityTestReportParser);
+    private final Parser<List<TestReport>> serenityTestReportsParser = new TestReportsParser(testReportParser, isSerenityResultsDirectory());
 
     @Test
     public void shouldParseTestReports() throws IOException {
-        when(serenityTestReportParser.parse(any(File.class))).thenReturn(new TestReport(asList(new TestResult("A", now(), SUCCESS))));
+        when(testReportParser.parse(any(File.class))).thenReturn(new TestReport(asList(new TestResult("A", now(), SUCCESS))));
 
         assertThat(serenityTestReportsParser.parse(new File("src/test")).isEmpty(), is(false));
     }
